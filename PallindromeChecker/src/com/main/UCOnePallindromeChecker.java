8131389adbcb4940b5b501649bc213f8eeb83f92
amitkr069 <aman3133@7-11.com>
2026-03-03 15:52:33 +0530

    System Initialized

---
+++ PallindromeChecker/src/com/main/UCOnePallindromeChecker.java
@@ -0,0 +1,9 @@
+package com.main;
+
+public class UCOnePallindromeChecker {
+	public static void main(String[] args) {
+		System.out.println("Welcome to pallindrome checker app");
+		System.out.println("Version 1.0");
+		System.out.println("System Initialized Successfully");
+	}
+}
